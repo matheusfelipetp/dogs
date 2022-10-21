2449df65a926646d4aa22906ed9fc6e1ed8b88c9
matheusfelipetp <matheusfelipetp@outlook.com>
2022-10-21 18:49:48 -0300

    feat: add Header, Footer, Home and Login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import { RoutesApp } from './routes';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <RoutesApp />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
