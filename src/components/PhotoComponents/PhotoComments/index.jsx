11886aeb688a7e50a5981d029e741c0e1fad93e8
matheusfelipetp <matheusfelipetp@outlook.com>
2022-10-26 18:09:38 -0300

    feat: add PhotoCommentsForm

--- src/components/PhotoComponents/PhotoComments/index.jsx
+++ src/components/PhotoComponents/PhotoComments/index.jsx
@@ -1,5 +1,25 @@
-const PhotoComments = () => {
-  return <div>PhotoComments</div>;
+import { useContext, useState } from 'react';
+import { UserContext } from '../../../context/UserContext';
+import PhotoCommentsForm from '../PhotoCommentsForm';
+import styles from './styles.module.css';
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const { login } = useContext(UserContext);
+
+  return (
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <strong>{comment.comment_author}: </strong>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
---
+++ src/components/PhotoComponents/PhotoCommentsForm/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { ReactComponent as Enviar } from '../../../assets/enviar.svg';
+import useFetch from '../../../hooks/useFetch';
+import { COMMENT_POST } from '../../../services/api';
+import MessageError from '../../MessageError';
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = useState('');
+  const { request, error } = useFetch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem('@dogs:token');
+    const { url, options } = COMMENT_POST(id, { comment }, token);
+    const { response, json } = await request(url, options);
+
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Digite seu comentário"
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+      <MessageError error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
